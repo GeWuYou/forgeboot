bf63580581e2783415494fec94c25a530e12beca
gewuyou <gewuyou1024@gmail.com>
2025-06-03 15:11:18 +0800

    feat(dto): Enrich data conversion interface and add list copy methods
    - Added copyList method to BaseMapper interface to convert object list in batch
    - Added partialUpdate method in the ConversionMapper interface to partially update entity objects
    - Optimized interface document annotations, clarifying the function and parameter meaning of the method
    - Adjusted the order of generic parameters to improve code readability

--- forgeboot-webmvc/dto/src/main/kotlin/com/gewuyou/forgeboot/webmvc/dto/mapper/ConversionMapper.kt
+++ forgeboot-webmvc/dto/src/main/kotlin/com/gewuyou/forgeboot/webmvc/dto/mapper/ConversionMapper.kt
@@ -1,10 +1,15 @@
-package com.gewuyou.forgeboot.webmvc.dto.dto.mapper
+package com.gewuyou.forgeboot.webmvc.dto.mapper
+
+import org.mapstruct.BeanMapping
+import org.mapstruct.MappingTarget
+import org.mapstruct.NullValuePropertyMappingStrategy
 
 /**
- * 转换 映射器
+ * 转换映射器接口
  *
- * 定义了一个转换映射器接口，用于在实体类和数据传输对象（DTO）之间进行转换。
- * 这个接口定义了四个基本转换方法：实体到DTO、DTO到实体、实体列表到DTO列表和DTO列表到实体列表。
+ * 提供通用的实体类(Entity)与数据传输对象(DTO)之间的双向转换能力。
+ * 该接口定义了基础的数据转换方法，包括单个对象和集合对象的转换，
+ * 并支持部分更新操作（忽略空值属性）。
  *
  * @param <Entity> 实体类类型
  * @param <Dto> 数据传输对象类型
@@ -12,36 +17,47 @@ package com.gewuyou.forgeboot.webmvc.dto.dto.mapper
  * @since 2025-05-30 22:53:35
  * @author gewuyou
  */
-interface ConversionMapper<Entity, Dto>{
+interface ConversionMapper<Entity, Dto> {
     /**
-     * 将实体对象转换为DTO对象
+     * 将实体对象转换为对应的DTO对象
      *
-     * @param entity 实体对象
+     * @param entity 需要转换的实体对象
      * @return 转换后的DTO对象
      */
     fun toDto(entity: Entity): Dto
 
     /**
-     * 将DTO对象转换为实体对象
+     * 将DTO对象转换为对应的实体对象
      *
-     * @param dto DTO对象
+     * @param dto 需要转换的DTO对象
      * @return 转换后的实体对象
      */
     fun toEntity(dto: Dto): Entity
 
     /**
-     * 将实体对象列表转换为DTO对象列表
+     * 将实体对象列表转换为对应的DTO对象列表
      *
-     * @param entityList 实体对象列表
+     * @param entityList 需要转换的实体对象列表
      * @return 转换后的DTO对象列表
      */
     fun toDtoList(entityList: List<Entity>): List<Dto>
 
     /**
-     * 将DTO对象列表转换为实体对象列表
+     * 将DTO对象列表转换为对应的实体对象列表
      *
-     * @param dtoList DTO对象列表
+     * @param dtoList 需要转换的DTO对象列表
      * @return 转换后的实体对象列表
      */
     fun toEntityList(dtoList: List<Dto>): List<Entity>
+
+    /**
+     * 使用非空属性对实体进行部分更新
+     *
+     * 注意：此操作不会覆盖实体中为空的属性
+     *
+     * @param dto 需要用于更新的DTO对象
+     * @param entity 需要被更新的实体对象
+     */
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    fun partialUpdate(dto: Dto, @MappingTarget entity: Entity)
 }
