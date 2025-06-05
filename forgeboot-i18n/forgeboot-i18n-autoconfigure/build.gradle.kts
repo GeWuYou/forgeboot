b90048a57d930dac756df4d458f150d50b541198
gewuyou <gewuyou1024@gmail.com>
2025-06-05 16:07:11 +0800

    build(forgeboot-i18n-autoconfigure): Update project dependency configuration - Move dependencies from the core-extension module to implementation configuration - Add spring-boot-autoconfigure dependencies - Update other dependencies to adapt to project structure changes

--- forgeboot-i18n/forgeboot-i18n-autoconfigure/build.gradle.kts
+++ forgeboot-i18n/forgeboot-i18n-autoconfigure/build.gradle.kts
@@ -1,10 +1,11 @@
 
 dependencies {
-    compileOnly(platform(libs.springBootDependencies.bom))
+    implementation(project(Modules.Core.EXTENSION))
+    implementation(platform(libs.springBootDependencies.bom))
+    implementation(libs.springBoot.autoconfigure)
     compileOnly(platform(libs.springCloudDependencies.bom))
     compileOnly(libs.springBootStarter.web)
     compileOnly(libs.springBootStarter.webflux)
     compileOnly(project(Modules.I18n.API))
     compileOnly(project(Modules.I18n.IMPL))
-    implementation(project(Modules.Core.EXTENSION))
 }
