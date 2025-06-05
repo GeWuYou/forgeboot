6942de1320fbdccdc5aa3eded45537473149afdf
gewuyou <gewuyou1024@gmail.com>
2025-06-05 16:09:08 +0800

    build(exception-i18n): Add Spring Boot Autoconfiguration Dependencies
    - Add Spring Boot dependency platform and autoconfiguration dependencies in the build.gradle.kts file
    - The addition of these dependencies helps to enhance project configuration and management capabilities

--- forgeboot-webmvc/exception-i18n/build.gradle.kts
+++ forgeboot-webmvc/exception-i18n/build.gradle.kts
@@ -4,6 +4,8 @@ plugins{
 }
 dependencies {
     implementation(project(Modules.Core.EXTENSION))
+    implementation(platform(libs.springBootDependencies.bom))
+    implementation(libs.springBoot.autoconfigure)
     api(project(Modules.I18n.STARTER))
     api(project(Modules.TRACE.STARTER))
     implementation(project(Modules.Webmvc.DTO))
