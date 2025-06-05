6c30120016e2f87c01389e3810cba9b1e99f513f
gewuyou <gewuyou1024@gmail.com>
2025-06-05 16:09:37 +0800

    build(forgeboot-webmvc): Add Spring Boot Dependencies
    - Introducing Spring Boot dependency management platform - Adding Spring Boot automatic configuration module
    - Keep existing project dependency structure unchanged

--- forgeboot-webmvc/version/build.gradle.kts
+++ forgeboot-webmvc/version/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
     implementation(project(Modules.Core.EXTENSION))
+    implementation(platform(libs.springBootDependencies.bom))
+    implementation(libs.springBoot.autoconfigure)
     kapt(libs.springBoot.configuration.processor)
     compileOnly(libs.springBootStarter.web)
 }
