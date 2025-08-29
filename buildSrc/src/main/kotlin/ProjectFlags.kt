91031bd63f74fc8a670302629ca64b40046f8548
gewuyou <gewuyou1024@gmail.com>
2025-08-29 17:12:50 +0800

    refactor(buildSrc): 添加项目配置常量并更新相关模块依赖- 在 ProjectFlags 对象中添加了三个新的常量标识符- 更新 forgeboot-plugin-demo-impl 模块的依赖，将 starter 替换为 core -优化 JimmerKtCrudServiceImplSpec 中的 pageWithView 函数，增加泛型约束

--- buildSrc/src/main/kotlin/ProjectFlags.kt
+++ buildSrc/src/main/kotlin/ProjectFlags.kt
@@ -1,5 +1,33 @@
+/*
+ *
+ *  * Copyright (c) 2025
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *  *
+ *
+ *
+ */
+
+/**
+ * 项目标志对象，包含用于项目配置的各种常量标识符
+ */
 object ProjectFlags {
+    /** 标识模块是否为根模块的属性键名 */
     const val IS_ROOT_MODULE = "isRootModule"
+
+    /** 标识模块是否为发布模块的属性键名 */
     const val IS_PUBLISH_MODULE = "isPublishModule"
+
+    /** 标识模块构件ID的属性键名 */
     const val ARTIFACT_ID = "artifactId"
 }
--- forgeboot-webmvc/spec-jimmer/src/main/kotlin/com/gewuyou/webmvc/spec/jimmer/service/impl/JimmerKtCrudServiceImplSpec.kt
+++ forgeboot-webmvc/spec-jimmer/src/main/kotlin/com/gewuyou/webmvc/spec/jimmer/service/impl/JimmerKtCrudServiceImplSpec.kt
@@ -1,3 +1,23 @@
+/*
+ *
+ *  * Copyright (c) 2025
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *  *
+ *
+ *
+ */
+
 package com.gewuyou.webmvc.spec.jimmer.service.impl
 
 import com.gewuyou.forgeboot.webmvc.dto.PageResult
@@ -31,18 +51,17 @@ open class JimmerKtCrudServiceImplSpec<Entity : Any, Id : Any>(
      * @param query 查询组件，包含分页和过滤条件
      * @return PageResult<V> 返回指定视图类型的分页结果
      */
-    inline fun <reified E : Any, reified V : View<E>> pageWithView(
-        query: QueryComponent,
-    ): PageResult<V> {
-        query as Pageable
+    inline fun <reified E : Any, reified V : View<E>, Q> pageWithView(
+        query: Q,
+    ): PageResult<V>
+            where Q : Pageable, Q : JimmerKFilterable<E>, Q : QueryComponent {
         val pageable = query.toPageRequest()
-        @Suppress("UNCHECKED_CAST")
-        query as JimmerKFilterable<E>
         return repository.sql.createQuery(E::class) {
             where(query.getSpecification())
             select(table.fetch(V::class))
         }.fetchSpringPage(pageable).toPageResult()
     }
+
 
     /**
      * 根据ID获取实体
